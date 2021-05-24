74bcba586bbb1939a48ab0b71ab9f8a7b9c7afe9
Rishav Sharma <rsrishav0@gmail.com>
2021-05-24 22:24:51 +0530

    Add files via upload

--- constants.py
+++ constants.py
@@ -1,10 +1,18 @@
 MY_COUNTRIES = ["Australia", "Brazil", "Canada", "France", "India", "Italy", "Japan", "Mexico", "United States",
                 "Germany"]
+
 WOEID_LIST = [23424748, 23424768, 23424775, 23424819, 23424829, 23424848, 23424853, 23424856, 23424900, 23424977]
+
 WOEID_DICT = {23424748: 'Australia', 23424768: 'Brazil', 23424775: 'Canada', 23424819: 'France',
               23424829: 'Germany', 23424848: 'India', 23424853: 'Italy', 23424976: 'Ukraine', 23424900: 'Mexico',
               23424977: 'United States'}
-TWEET_HEADERS = ["tweet_id", "tweet_datetime", "tweet_hashtags", "tweet_language", "retweet_count",
+
+# WOEID_DICT = {23424748: 'Australia', 23424768: 'Brazil'}
+
+TWEET_COLUMNS = ["tweet_id", "tweet_datetime", "tweet_hashtags", "tweet_language", "retweet_count",
                  "tweet_source", "tweet_source_url", "tweet_text", "user_created_datetime", "user_name",
                  "user_followers_count", "user_description", "user_location",
                  "searched_by_hashtag", "searched_at_datetime", "searched_hashtag_country"]
+
+TREND_COLUMNS = ["trend_name", "trend_url", "trend_query", "tweet_volume",
+                 "searched_at_datetime", "searched_in_country"]
--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import pandas as pd
 from constants import *
 from datetime import datetime
 from kaggle import KaggleApi as kag_api
-# import twitter
-# import geocoder
 
 DATASET_NAME = "twitter-trending-tweets"
 DATA_FOLDER = "datasets"
 
 CURRENT_DATETIME = datetime.utcnow()
 TWEET_FILE_PATH = f"{DATA_FOLDER}/{CURRENT_DATETIME.year}_{CURRENT_DATETIME.strftime('%B')}_twitter_trending_data.csv"
+ALL_TREND_FILE_PATH = f"{DATA_FOLDER}/{CURRENT_DATETIME.year}_{CURRENT_DATETIME.strftime('%B')}_all_trends_data.csv"
+HASHTAG_TREND_FILE_PATH = f"{DATA_FOLDER}/{CURRENT_DATETIME.year}_{CURRENT_DATETIME.strftime('%B')}_hashtag_trend_data.csv"
 
 CONSUMER_KEY = os.environ["CONSUMER_KEY"]
 CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
@@ -41,10 +41,6 @@ def twitter_authenticate():
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-
-    # auth2 = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
-    #                             CONSUMER_KEY, CONSUMER_SECRET)
-    # twitter_api = twitter.Twitter(auth=auth2)
     print("[INFO] Twitter api authenticated.")
     return api
 
@@ -52,24 +48,28 @@ def twitter_authenticate():
 def kaggle_authenticate():
     api = kag_api()
     kag_api.authenticate(api)
-    print("[INFO] Kaggle api authenticated.")
+    print("\n[INFO] Kaggle api authenticated.")
     return api
 
 
 def get_trends(api):
-    country_wise_trends = dict()
+    top_10_trends = dict()
+    all_trends_hashtags = dict()
+    all_trends = dict()
     for key, value in WOEID_DICT.items():
         print(f"[INFO] Fetching twitter trends for country: {value}")
         trends = api.trends_place(key)
+        all_trends[value] = trends[0]["trends"]
         trend_hashtags = [trend for trend in trends[0]["trends"] if trend["name"][0] == "#"]
+        all_trends_hashtags[value] = trend_hashtags
         top_10 = trend_hashtags[:10] if len(trend_hashtags) > 10 else trend_hashtags
-        country_wise_trends[value] = top_10
+        top_10_trends[value] = top_10
         print(f"> Trends fetched (max 10): {len(top_10)}\n")
         time.sleep(1)
-    return country_wise_trends
+    return top_10_trends, all_trends_hashtags, all_trends
 
 
-def get_tweets(api, country_wise_trends):
+def generate_tweet_data(api, country_wise_trends):
     tweet_data = list()
     # country_wise_trends = get_trends(api)
     for country, trends in country_wise_trends.items():
@@ -86,9 +86,18 @@ def get_tweets(api, country_wise_trends):
     return tweet_data
 
 
-def convert_to_df(data):
-    df = pd.DataFrame(data, columns=TWEET_HEADERS, dtype=str)
-    print(df)
+def generate_trends_data(country_trends):
+    trend_data = list()
+    for country, trends in country_trends.items():
+        for trend in trends:
+            d = [trend["name"], trend["url"], trend["query"], trend["tweet_volume"], CURRENT_DATETIME, country]
+            trend_data.append(d)
+    return trend_data
+
+
+def convert_to_df(data, columns):
+    df = pd.DataFrame(data, columns=columns, dtype=str)
+    # print(df)
     return df
 
 
@@ -103,17 +112,26 @@ def kaggle_dataset_download(api, dataset_name, path):
 
 def kaggle_upload_dataset(api, path):
     kag_api.dataset_create_version(api, path, f"Dataset updated till (UTC): {datetime.utcnow()}",
-                                   convert_to_csv=True, delete_old_versions=False)
-    print("[INFO] Dataset uploaded.")
+                                   convert_to_csv=True, delete_old_versions=True)
+    print("\n[INFO] Dataset uploaded.\n")
     clear_dir(path)
 
 
 if __name__ == '__main__':
     t_api = twitter_authenticate()
-    country_wise_trends = get_trends(t_api)
-    tweet_data = get_tweets(t_api, country_wise_trends)
-    df = convert_to_df(tweet_data)
+    top_10, hashtag_trends, all_trends = get_trends(t_api)
+
+    all_trends_data = generate_trends_data(all_trends)
+    hashtag_trends_data = generate_trends_data(hashtag_trends)
+    all_trends_df = convert_to_df(all_trends_data, TREND_COLUMNS)
+    hashtag_trends_df = convert_to_df(hashtag_trends_data, TREND_COLUMNS)
+
+    tweet_data = generate_tweet_data(t_api, top_10)
+    tweet_df = convert_to_df(tweet_data, TWEET_COLUMNS)
+
     k_api = kaggle_authenticate()
     kaggle_dataset_download(k_api, DATASET_NAME, DATA_FOLDER)
-    save_df_csv(df, TWEET_FILE_PATH)
+    save_df_csv(all_trends_df, ALL_TREND_FILE_PATH)
+    save_df_csv(hashtag_trends_df, HASHTAG_TREND_FILE_PATH)
+    save_df_csv(tweet_df, TWEET_FILE_PATH)
     kaggle_upload_dataset(k_api, DATA_FOLDER)
